3d3d1e2322d47185101f47b3b7cecc6ebc702ae8
majon911 <majonez202@gmail.com>
2023-04-29 12:31:48 +0200

    Added mechanics - 29/04/23

---
+++ mechanics.py
@@ -0,0 +1,41 @@
+import random
+import zombie_simulation_2 as zs
+import concurrent.futures
+
+citizen_queue_init = []
+citizen_id = 0
+for i in range(1000):
+    city_prob = random.choices(["Mackers City", 'Gulans Town', 'Nogales Village', 'Albono Hills', 'Zeidel Borough'], [3, 2, 2, 1, 2])
+    citizen_queue_init.append(zs.Citizen(citizen_id, city_prob))
+    citizen_id = citizen_id + 1
+
+military_queue_init = []
+military_id = 0
+for i in range(100):
+    city_prob = random.choices(["Mackers City", 'Gulans Town', 'Nogales Village', 'Albono Hills', 'Zeidel Borough'], [3, 2, 2, 1, 2])
+    mtype = random.choices(["Soldier", "Soldier Armoured", "Tank", "Plane"], [5, 4, 2, 1])
+    mrank = random.choices([1, 2, 3], [6, 3, 1])
+    military_queue_init.append(zs.Military(military_id, mtype, mrank, city_prob))
+    military_id = military_id + 1
+
+medic_queue_init = []
+medic_id = 0
+for i in range(50):
+    city_prob = random.choices(["Mackers City", 'Gulans Town', 'Nogales Village', 'Albono Hills', 'Zeidel Borough'], [3, 2, 2, 1, 2])
+    medic_queue_init.append(zs.Medic(medic_id, "Medic", city_prob))
+    medic_id = medic_id + 1
+
+
+with concurrent.futures.ThreadPoolExecutor(max_workers=1000) as executor:
+    for citizen in citizen_queue_init:
+        executor.submit(citizen.zombify)
+
+with concurrent.futures.ThreadPoolExecutor(max_workers=100) as executor:
+    for personnel in military_queue_init:
+        executor.submit(personnel.zombie_destruction)
+        executor.submit(personnel.military_zombify)
+
+with concurrent.futures.ThreadPoolExecutor(max_workers=50) as executor:
+    for medic in medic_queue_init:
+        executor.submit(medic.zombie_cure)
+        executor.submit(medic.medic_zombify)
--- zombie_simulation_2.py
+++ zombie_simulation_2.py
@@ -4,6 +4,7 @@ import time
 import webbrowser
 import random
 import math
+import concurrent.futures
 
 total_population = 0
 total_zombies = 0
@@ -502,6 +503,49 @@ def city_checker():
     else:
         print("Invalid Input. ")
         city_checker()
+
+#######################################################################
+## MECHANICS ##
+
+citizen_queue_init = []
+citizen_id = 0
+for i in range(1000):
+    city_prob = random.choices(["Mackers City", 'Gulans Town', 'Nogales Village', 'Albono Hills', 'Zeidel Borough'],
+                               [3, 2, 2, 1, 2])
+    citizen_queue_init.append(Citizen(citizen_id, city_prob))
+    citizen_id = citizen_id + 1
+
+military_queue_init = []
+military_id = 0
+for i in range(100):
+    city_prob = random.choices(["Mackers City", 'Gulans Town', 'Nogales Village', 'Albono Hills', 'Zeidel Borough'],
+                               [3, 2, 2, 1, 2])
+    mtype = random.choices(["Soldier", "Soldier Armoured", "Tank", "Plane"], [5, 4, 2, 1])
+    mrank = random.choices([1, 2, 3], [6, 3, 1])
+    military_queue_init.append(Military(military_id, mtype, mrank, city_prob))
+    military_id = military_id + 1
+
+medic_queue_init = []
+medic_id = 0
+for i in range(50):
+    city_prob = random.choices(["Mackers City", 'Gulans Town', 'Nogales Village', 'Albono Hills', 'Zeidel Borough'],
+                               [3, 2, 2, 1, 2])
+    medic_queue_init.append(Medic(medic_id, "Medic", city_prob))
+    medic_id = medic_id + 1
+
+with concurrent.futures.ThreadPoolExecutor(max_workers=1000) as executor:
+    for citizen in citizen_queue_init:
+        executor.submit(citizen.zombify)
+
+with concurrent.futures.ThreadPoolExecutor(max_workers=100) as executor:
+    for personnel in military_queue_init:
+        executor.submit(personnel.zombie_destruction)
+        executor.submit(personnel.military_zombify)
+
+with concurrent.futures.ThreadPoolExecutor(max_workers=50) as executor:
+    for medic in medic_queue_init:
+        executor.submit(medic.zombie_cure)
+        executor.submit(medic.medic_zombify)
 
 
 # def simulation():
